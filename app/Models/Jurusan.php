b7266a0b880a0b791aaec62d1c7ec450ef4613fa
Deva <theprojessa@gmail.com>
2022-05-03 22:45:45 +0700

    penggunaaan sederhana one to many relasi database

---
+++ app/Models/Jurusan.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jurusan extends Model
+{
+    use HasFactory;
+
+    public function mahasiswas()
+    {
+        return $this->hasMany('App\Models\Mahasiswa');
+    }
+}
